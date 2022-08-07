feba16281e6c95728db6b608de8b491fa0ea249a
danielwsilva <danielwllace@gmail.com>
2022-08-07 14:37:37 -0300

    Theme created

---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export default {
+  colors: {
+    // Basic colors
+    white: '#FFFFFF',
+
+    // Primary colors
+    primary: '#43d7c2',
+    secondary: '#3882e1',
+
+    // RGBA
+    gradientPrimary: 'rgba(96,218,201,0.7)',
+    gradientSecondary: 'rgba(56,130,225,0.7)', 
+    swipeDelete: 'rgba(96,218,201, .3)',
+    swipeEdit: 'rgba(56,130,225, .3)',
+
+    // Status Colors
+    success: '#5CB85C',
+    danger: '#EB0E0F',
+    error: '#EF0C0C',
+
+    // Gray colors
+    text: '#686868',
+    textLight: '#989898',
+    lightGray: '#E7E7E7',
+
+    // Transparent color
+    none: 'transparent'
+  },
+
+  fonts: {
+    primary_400: 'Manrope_400Regular',
+    primary_600: 'Manrope_600SemiBold',
+    primary_700: 'Manrope_700Bold',
+  }
+};
